f000cedb89b2da57035d1e823ed18fbd14165192
jaystas <jays.staigers@gmail.com>
2026-05-28 05:18:51 -0500

    styles

--- .interface/src/pages/jobs/find/FindPage.tsx
+++ .interface/src/pages/jobs/find/FindPage.tsx
@@ -5,8 +5,6 @@ import "./find.css"
 export function FindPage() {
   return (
     <section className="find-page">
-      <header className="find-page-header">
-      </header>
 
       <JobsGrid />
     </section>
